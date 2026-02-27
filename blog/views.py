5aef924c9f111b1d79f98f1327f0086c3ff4e981
Priyabrat Swain <84319674+priyabrat8@users.noreply.github.com>
2026-02-27 22:23:31 +0530

    add absolute url and pagination

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(
         request,
         'blog/post/list.html',
         {'posts': posts}
     )
 
-def post_detail(request, id):
+def post_detail(request,year, month, day, post):
     post = get_object_or_404(
         Post,
-        id=id,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
         status=Post.Status.PUBLISHED
     )
     return render(
