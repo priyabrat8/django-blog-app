ff25e2a4e771b0f644c6ff6352111833f7499dc1
Priyabrat Swain <84319674+priyabrat8@users.noreply.github.com>
2026-03-11 15:57:46 +0530

    Add Email Sending Features and Frontent Form

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 from .models import Post
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
@@ -32,3 +34,41 @@ def post_detail(request,year, month, day, post):
         'blog/post/detail.html',
         {'post': post}
     )
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(
+    Post,
+    id=post_id,
+    status=Post.Status.PUBLISHED
+    )
+    sent = False 
+    form = EmailPostForm()
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+    if form.is_valid():
+        # Form fields passed validation
+        cd = form.cleaned_data
+        post_url = request.build_absolute_uri(
+        post.get_absolute_url()
+        )
+        subject = (
+        f"{cd['name']} ({cd['email']}) "
+        f"recommends you read {post.title}"
+        )
+        message = (
+        f"Read {post.title} at {post_url}\n\n"
+        f"{cd['name']}\'s comments: {cd['comments']}"
+        )
+        send_mail(
+        subject=subject,
+        message=message,
+        from_email=None,
+        recipient_list=[cd['to']]
+        )
+        sent = True
+    else:
+        form = EmailPostForm()
+    return render(request,'blog/post/share.html',{'post': post,'form': form, 'sent': sent})
+        
