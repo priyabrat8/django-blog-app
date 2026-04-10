20b66f64e30a8a5814cc008493f09d84a36a76b9
Priyabrat Swain <84319674+priyabrat8@users.noreply.github.com>
2026-04-10 18:39:49 +0530

    add tag,related post features

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,17 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from .models import Post
+from taggit.models import Tag
+from django.db.models import Count
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     try:
@@ -18,7 +25,7 @@ def post_list(request):
     return render(
         request,
         'blog/post/list.html',
-        {'posts': posts}
+        {'posts': posts, 'tag': tag}
     )
 
 def post_detail(request,year, month, day, post):
@@ -35,11 +42,20 @@ def post_detail(request,year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+    tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+    same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
     
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post, 'comments': comments, 'form': form}
+        {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts}
     )
 
 def post_share(request, post_id):
